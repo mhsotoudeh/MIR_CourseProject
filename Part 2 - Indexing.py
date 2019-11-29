d057a5a0debbc8527c6904e999ef7089c49ea123
Parham Ghasemloo <parhamghasemloo75@gmail.com>
2019-11-29 13:23:59 +0330

    tweaks to indexing - dict mode and persian support

--- Part 2 - Indexing.py
+++ Part 2 - Indexing.py
@@ -1,3 +1,6 @@
+import json
+
+
 class PositionalPostingNode:
 
     def __init__(self, trie_node, doc_id, position, prev=None):
@@ -52,13 +55,15 @@ class TrieNode:
     def add_word(self, word, doc_id, position, char_index=0):
         if char_index == len(word):
             if self.posting_list is None:
-                print('initializing word:', word)
+                # print('initializing word:', word)
                 self.posting_list = PositionalPostingNode(self, None, None)
 
             if doc_id not in TrieNode.DOCS:
-                print('new doc')
+                # print('new doc')
                 TrieNode.DOCS[doc_id] = set()
+
             TrieNode.DOCS[doc_id].add(self.posting_list.add_entry(doc_id, position))
+
             if word not in TrieNode.WORDS:
                 TrieNode.WORDS.append(word)
                 Bigrams.add_word(word)
@@ -83,8 +88,12 @@ class TrieNode:
                 print("word doesn't exist:", word)
                 return
         if iter_node.posting_list is not None and iter_node.word == word:
-            print("word found:", word)
-            return iter_node.posting_list.next
+            out = dict()
+            iter_node = iter_node.posting_list.next
+            while iter_node is not None:
+                out[iter_node.doc_id] = iter_node.positions
+                iter_node = iter_node.next
+            return out
         else:
             # should never reach here
             print("(shouldn't reach here) word doesn't exist:", word)
@@ -96,19 +105,41 @@ class TrieNode:
                 del self.parent[0].children[self.parent[1]]
                 self.parent[0].check_valid()
 
+    def to_dict(self):
+        out = dict()
+        for word in TrieNode.WORDS:
+            out[word] = self.get_postings_list(word)
+        return out
+
+    @staticmethod
+    def from_dict(inp):
+        TrieNode.DOCS = dict()
+        TrieNode.WORDS = list()
+        TrieNode.DICT_MODE = dict()
+        _trie = TrieNode()
+        try:
+            for word in inp:
+                for doc_id in inp[word]:
+                    for position in inp[word][doc_id]:
+                        _trie.add_word(word, doc_id, position)
+        except KeyError:
+            print('wrong input format')
+        return _trie
+
 
 class Bigrams:
     _bigrams = dict()
-    letters = 'abcdefghijklmnopqrstuvwxyz'
+    letters = 'abcdefghijklmnopqrstuvwxyzابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی'
     for c_1 in letters:
         _bigrams[c_1] = dict()
         for c_2 in letters:
-            _bigrams[c_1][c_2] = set()
+            _bigrams[c_1][c_2] = []
 
     @staticmethod
     def add_word(word):
         for bigram in find_word_bigrams(word):
-            Bigrams._bigrams[bigram[0]][bigram[1]].add(word)
+            if word not in Bigrams._bigrams[bigram[0]][bigram[1]]:
+                Bigrams._bigrams[bigram[0]][bigram[1]].append(word)
 
     @staticmethod
     def remove_word(word):
@@ -124,7 +155,17 @@ class Bigrams:
                     results[_w] = 1
                 else:
                     results[_w] += 1
+        for _w in results:
+            results[_w] /= (len(_w) + len(word) - 2 - results[_w])
         return results
+
+    @staticmethod
+    def to_dict():
+        return Bigrams._bigrams
+
+    @staticmethod
+    def from_dict(inp):
+        Bigrams._bigrams = inp
 
 
 def find_word_position(tokenized_document, index):
@@ -153,8 +194,30 @@ def find_document_bigrams(tokenized_document):
     return document_bigrams
 
 
-d = ['i', 'akbar', 'is', 'lazy', 'islam', 'is']
-f = ['akbar']
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[
+                             j + 1] + 1  # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1  # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
+d = ['i', 'اکبر', 'is', 'lazy', 'islam', 'is']
+f = ['اکبر']
 wr = 'module'
 
 trie = TrieNode()
@@ -163,25 +226,6 @@ for w in range(len(d)):
 for w in range(len(f)):
     trie.add_word(f[w], 'f', w)
 
-print(trie.children)
-print()
-print(TrieNode.DOCS)
-print(TrieNode.WORDS)
-print()
-trie.get_postings_list('akbar')
-print(trie.get_postings_list('is').positions)
-print(find_word_bigrams('akhbar'))
-print(find_word_bigrams('akbar'))
-print(Bigrams.get_similar_words('islav'))
-
-for doc in TrieNode.DOCS['d']:
-    doc.remove()
-del TrieNode.DOCS['d']
-
-print()
-trie.get_postings_list('i')
-print(trie.children['a'].children)
-print(TrieNode.DOCS)
-print(TrieNode.WORDS)
-print(Bigrams.get_similar_words('islav'))
-print(Bigrams.get_similar_words('akaber'))
+store_file = open('store_file', 'w', encoding='utf8')
+json.dump(trie.to_dict(), store_file, ensure_ascii=False)
+store_file.close()
